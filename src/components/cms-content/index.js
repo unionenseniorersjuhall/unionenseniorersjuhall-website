f8ca15d5480e2e3b22ef58be5c8a6f65dfe4751d
NaNdreas <andreas.nilsson333@gmail.com>
2018-02-17 10:39:22 +0100

    cms content

--- src/components/cms-content/index.js
+++ src/components/cms-content/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
 export default function CmsContent(props) {
-  console.log(props);
   return (
-    <div>cms content</div>
+    <div dangerouslySetInnerHTML={{ __html: props.html }} />
   );
 }
