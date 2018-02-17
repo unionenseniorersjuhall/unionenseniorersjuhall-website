cf2583b3d892f6091416eec77d01bf55c41a627a
NaNdreas <andreas.nilsson333@gmail.com>
2018-02-17 20:26:38 +0100

    add title

--- src/components/cms-content/index.js
+++ src/components/cms-content/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function CmsContent(props) {
   return (
-    <div className="cms-content" dangerouslySetInnerHTML={{ __html: props.body }} />
+    <div className="cms-content">
+      <h1>{props.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: props.body }} />
+    </div>
   );
 }
