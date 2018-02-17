f7630f463e682f559f01069e938b3b43176810cc
NaNdreas <andreas.nilsson333@gmail.com>
2018-02-17 13:11:34 +0100

    config

--- get-preloaded-state.js
+++ get-preloaded-state.js
@@ -3,29 +3,21 @@ const path = require('path');
 const metaMarked = require('meta-marked');
 
 function getPreloadedState() {
-  const settings = require(path.resolve('./src/data/_settings/general.json'));
-  const homepage = require(path.resolve('./src/data/_homepage/homepage.json'));
-  const pages = fs.readdirSync(path.resolve('./src/data/_pages'))
-    .map(filename => filename.replace('.md', ''))
-    .map((page) => {
-      const mdContent = fs.readFileSync(path.resolve(`./src/data/_pages/${page}.md`)).toString();
-      const markup = metaMarked(mdContent);
+  const settings = require('./src/data/_settings/general.json');
+  const homepage = require('./src/data/_homepage/homepage.json');
+  homepage.body = metaMarked(homepage.body).html;
 
-      return ({
-        route: (settings.homepage === page) ? '/' : `/${page}`,
-        data: markup.meta,
-        html: markup.html,
-      });
+  const pages = fs.readdirSync(path.resolve('./src/data/_pages'))
+    .map(filename => filename.replace('.json', ''))
+    .map((page) => {
+      const pageData = require(`./src/data/_pages/${page}.json`);
+      pageData.body = metaMarked(pageData.body).html;
+      pageData.route = `/${page}`;
+
+      return pageData;
     });
 
-  return ({
-    homepage: {
-      data: homepage,
-      html: metaMarked(homepage.body).html,
-    },
-    pages,
-    settings,
-  });
+  return ({ homepage, pages, settings });
 }
 
 module.exports = getPreloadedState;
