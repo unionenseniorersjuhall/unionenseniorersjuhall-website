6e3c58f2785bb384ec27419be5ad28bafffb6587
NaNdreas <andreas.nilsson333@gmail.com>
2018-02-27 15:53:34 +0100

    add cms content in image gallery

--- src/pages/image-gallery.js
+++ src/pages/image-gallery.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import CmsContent from '../components/cms-content';
 
-function ImageGallery({ galleries }) {
-  return Object.entries(galleries).map(([key, object]) => (
+function ImageGallery({ content, galleries }) {
+  return (
     <div>
-      <h3>{object.title}</h3>
-      {object.images.map(img => (
-        <img key={img.src} src={img.src} />
+      <CmsContent>{content}</CmsContent>
+      {Object.entries(galleries).map(([key, object]) => (
+        <div key={key}>
+          <h3>{object.title}</h3>
+          {object.images.map(img => (
+            <img key={img.src} src={img.src} />
+          ))}
+        </div>
       ))}
     </div>
-  ));
+  );
 }
 
 const mapStateToProps = state => ({
   galleries: state.pages.imageGallery.galleries,
-  body: state.pages.imageGallery.body,
+  content: state.pages.imageGallery.content,
 });
 
 export default connect(mapStateToProps)(ImageGallery);
