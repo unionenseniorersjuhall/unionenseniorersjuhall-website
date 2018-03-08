227f19f6ea176b0d2ac1e02a74c6a1fe4cc2dae1
NaNdreas <andreas.nilsson333@gmail.com>
2018-03-08 22:40:09 +0100

    navigation updates

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -5,10 +5,10 @@ export default function Navigation() {
   return (
     <nav className="navigation">
       <div className="navigation__inner">
-        <NavLink className="navigation__link" to="/">Hem</NavLink>
-        <NavLink className="navigation__link" to="/program">Program</NavLink>
-        <NavLink className="navigation__link" to="/bildgalleri">Bildgalleri</NavLink>
-        <NavLink className="navigation__link" to="/kontakta-oss">Kontakta oss</NavLink>
+        <NavLink className="navigation__link" exact to="/">Hem</NavLink>
+        <NavLink className="navigation__link" exact to="/program">Program</NavLink>
+        <NavLink className="navigation__link" exact to="/bildgalleri">Bildgalleri</NavLink>
+        <NavLink className="navigation__link" exact to="/kontakta-oss">Kontakta oss</NavLink>
       </div>
     </nav>
   );
