44f5c30c71a15cdd0156c4daa3ba82e9472845e3
NaNdreas <andreas.nilsson333@gmail.com>
2018-03-21 19:10:36 +0100

    list images in grid

--- src/components/image-gallery/index.js
+++ src/components/image-gallery/index.js
@@ -4,11 +4,13 @@ function ImageGallery({ images, title }) {
   return (
     <div className="image-gallery">
       <h1 className="image-gallery__title">{title}</h1>
-      {images.map(image => (
-        <div className="image-gallery__item" key={image.src}>
-          <img className="image-gallery__image" src={image.src} alt="" />
-        </div>
-      ))}
+      <ul className="image-gallery__images">
+        {images.map(image => (
+          <li className="image-gallery__item" key={image.src}>
+            <img className="image-gallery__image" src={image.src} alt="" />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
