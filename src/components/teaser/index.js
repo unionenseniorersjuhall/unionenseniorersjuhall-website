b39e152d975d3dbdd212616cd43e74205ac1db9b
NaNdreas <andreas.nilsson333@gmail.com>
2018-04-30 21:21:53 +0200

    quotes in src

--- src/components/teaser/index.js
+++ src/components/teaser/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Teaser({ src, alt, title, subTitle, href }) {
   return (
     <Link to={href} className="teaser">
-      <div className="teaser__image" style={{ backgroundImage: `url(${src})` }}>
+      <div className="teaser__image" style={{ backgroundImage: `url("${src}")` }}>
         <div className="teaser__cta">Se bilder</div>
       </div>
       <h3 className="teaser__title">{title}</h3>
