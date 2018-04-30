6deba33c9426862f85c668b85bc6d0c699d7c801
NaNdreas <andreas.nilsson333@gmail.com>
2018-04-30 21:25:28 +0200

    show the right images in teasers

--- src/components/teaser-list/index.js
+++ src/components/teaser-list/index.js
@@ -4,8 +4,8 @@ import Teaser from '../teaser';
 function TeaserList({ gallery }) {
   return (
     <div className="teaser-list">
-      {Object.entries(gallery).map(([key, object]) => (
-        <Teaser key={key} href={`/bildgalleri/${key}`} src={object.images[0].src.replace('.jpg', '_medium.jpg').replace('.png', '_medium.png').substr(1)} title={object.title} />
+      {Object.entries(gallery).map(([key, object], i) => (
+        <Teaser key={key} href={`/bildgalleri/${key}`} src={object.images[i].src.replace('.jpg', '_medium.jpg').replace('.png', '_medium.png').substr(1)} title={object.title} />
       ))}
     </div>
   );
